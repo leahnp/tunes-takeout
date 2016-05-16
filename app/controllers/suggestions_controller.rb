f1aac24fa0616fb1b0d0621cade759332dfb6d79
Leah <leahnpetersen@gmail.com>
2016-05-16 16:58:15 -0700

    added form to suggestions index, not hooked up yet, added nav to layouts - currently the paths are breaking the app

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,18 @@
 class SuggestionsController < ApplicationController
+
+# index: shows top 20 suggestions, ranked by total number of favorites
+  def index
+  end
+
+# favorites: shows all suggestions favorited by the signed-in User
+  def favorites
+  end
+
+# favorite: adds a suggestion into the favorite list for the signed-in User. This requires interaction with the Tunes & Takeout API.
+  def favorite
+  end
+
+# unfavorite: removes a suggestion from the favorite list for the signed-in User. This requires interaction with the Tunes & Takeout API.
+  def unfavorite
+  end
 end
