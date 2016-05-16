8e5b1ac2bf6ecd0a0a59c0108a30d4f1c123bfa6
Leah <leahnpetersen@gmail.com>
2016-05-16 16:12:30 -0700

    added controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+
+
+#   new: shows a view with OAuth sign-in link
+  def new
+  end
+# create: accepts OAuth information from Spotify, finds or creates a User account, and sets user_id in session
+  def create
+  end
+  
+# destroy: deletes user_id from session
+  def destroy
+  end
+
+
+end
