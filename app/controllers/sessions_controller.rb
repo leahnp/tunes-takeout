0302f06916f5920a2700f23679c582c085bebafc
Leah <leahnpetersen@gmail.com>
2016-05-17 16:11:33 -0700

    added create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,22 @@ class SessionsController < ApplicationController
   def new
   end
 # create: accepts OAuth information from Spotify, finds or creates a User account, and sets user_id in session
-  def create
-  end
+    def create
+    auth_hash = request.env['omniauth.auth']
+    raise
+    @user = User.find_or_create_from_omniauth(auth_hash)
+  #   if auth_hash["uid"]
+  #     @user = User.find_or_create_from_omniauth(auth_hash)
+      if @user
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        redirect_to root_path, notice: "Failed to save the user"
+      end
+  #   else
+  #     redirect_to root_path, notice: "Failed to authenticate"
+    end
+
   
 # destroy: deletes user_id from session
   def destroy
