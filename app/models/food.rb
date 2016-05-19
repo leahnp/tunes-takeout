88f110fbd5709365d669a2f54893bc39590f2e08
Leah <leahnpetersen@gmail.com>
2016-05-19 14:14:01 -0700

    initializing url, image, phone, addy for yelp businesses

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,13 +6,15 @@ class Food < ActiveRecord::Base
   #urban-curry-san-francisco?actionlinks=True
   # get from yelp
 
-  attr_reader :name
+  attr_reader :name, :url, :image_url, :phone, :address
   FOOD_OPTIONS = []
 
   def initialize(data)
     @name = data.business.name
-      # @yelp_data.business.name
-
+    @url = data.business.url
+    @image_url = data.business.image_url
+    @phone = data.business.phone
+    @address = data.business.location.display_address
   end
 
   def self.find(biz_id_array)
@@ -23,6 +25,8 @@ class Food < ActiveRecord::Base
     biz_id_array.each do |biz_id|
 
       data = Yelp.client.business(biz_id)
+      raise
+
     # @yelp_data = HTTParty.get(BASE_URL + biz_id).parsed_response
     # @yelp_data = HTTParty.get("https://api.yelp.com/v2/business/yelp-san-francisco").parsed_response
     
