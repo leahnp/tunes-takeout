83170429d36a13bf3a97dc330dbab3c5f4220781
Leah <leahnpetersen@gmail.com>
2016-05-20 11:21:09 -0700

    refactored yelp info from chuck api to be hash

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -6,24 +6,24 @@ class SuggestionsController < ApplicationController
 
 # index: shows top 20 suggestions, ranked by total number of favorites
   def index
-    @yelp = []
-    @spotify_array = []
+    # @yelp = []
+    # @spotify_array = []
     @top_pairs = TunesTakeoutWrapper.top(9)
-    @top_pairs.each do |pair|
-      @yelp << TunesTakeoutWrapper.get_yelp_array([pair["suggestion"]])[0]
-      @spotify_array << TunesTakeoutWrapper.get_spotify_arrays([pair["suggestion"]])[0]
-    end
-    @yelp_business_names = Food.find(@yelp)
-    @spotify_info = Music.find(@spotify_array)
+    # @top_pairs.each do |pair|
+    #   @yelp << TunesTakeoutWrapper.get_yelp_array([pair["suggestion"]])[0]
+    #   @spotify_array << TunesTakeoutWrapper.get_spotify_arrays([pair["suggestion"]])[0]
+    # end
+    # @yelp_business_names = Food.find(@yelp)
+    # @spotify_info = Music.find(@spotify_array)
   end
 
   def show
     # keyword = params[:keyword]
     @results = TunesTakeoutWrapper.search(params[:keyword])
-    @yelp = TunesTakeoutWrapper.get_yelp_array(@results["suggestions"])
-    @spotify_array = TunesTakeoutWrapper.get_spotify_arrays(@results["suggestions"])
-    @yelp_business_names = Food.find(@yelp)
-    @spotify_info = Music.find(@spotify_array)
+    @yelp = TunesTakeoutWrapper.get_yelp_hash(@results["suggestions"])
+    # @spotify_array = TunesTakeoutWrapper.get_spotify_arrays(@results["suggestions"])
+    # @yelp_business_names = Food.find(@yelp)
+    # @spotify_info = Music.find(@spotify_array)
     # redirect_to search_path
     render :show
   end
--- lib/TunesTakeoutWrapper.rb
+++ lib/TunesTakeoutWrapper.rb
@@ -1,41 +1,5 @@
 require 'httparty'
 
-# class ChuckApp
-#   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
-#   attr_reader 
-
-#   def initialize(data)
-#     # @name = data["name"]
-#     # @moves = data["moves"]
-#     # @sprites = reject_null_sprites(data["sprites"])
-#   end
-
-#   # def sprite_urls
-#   #   sprites.values
-#   # end
-
-#   # def favorite_move
-#   #   HTTParty.get(moves.sample["move"]["url"]).parsed_response
-#   # end
-
-#   def self.find(id)
-#     # http://pokeapi.co/api/v2/pokemon/25/
-#     # find a pokemon with the passed id
-#     data = HTTParty.get(BASE_URL + "v1/search?query=avocado&limit=3").parsed_response
-
-#     # return an instance of pokemon for that id
-#     self.new(data)
-#   end
-
-#   # private
-
-#   # def reject_null_sprites(sprite_hash)
-#   #   sprite_hash.reject do |name, url|
-#   #     url.nil?
-#   #   end
-#   # end
-# end
-# module - a class without instance methods???
 module TunesTakeoutWrapper
   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
   # def initialize(data)
@@ -45,12 +9,16 @@ module TunesTakeoutWrapper
     @data = HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
   end
 
-  def self.get_yelp_array(suggestions)
-    yelp_array = []
+  def self.get_yelp_hash(suggestions)
+    # hash with suggestion id as key and value is hash with food_id key, id value pair
+    yelp_hash = {}
     suggestions.each do |s|
-      yelp_array << s["food_id"]
+      yelp_hash[s["id"]] = {
+        food_id: s["food_id"]
+      }
     end
-    return yelp_array
+    raise
+    return yelp_hash
   end
 
   def self.get_spotify_arrays(suggestions)
