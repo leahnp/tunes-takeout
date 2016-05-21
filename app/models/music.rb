22698c783980951f69aea96dc84b84b1210cd8a1
Leah <leahnpetersen@gmail.com>
2016-05-21 10:48:36 -0700

    added external url to link to music info on spotify

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,5 +1,5 @@
 class Music < ActiveRecord::Base
-  attr_reader :name, :images_array, :uri, :type
+  attr_reader :name, :images_array, :uri, :type, :url
   SPOTIFY_INSTANCES = []
 
 
@@ -7,6 +7,7 @@ class Music < ActiveRecord::Base
     @name = data.name
     @uri = data.uri
     @type = data.type
+    @url = data.external_urls["spotify"]
     if data.type == "track"
       album = data.album
       @images_array = album.images
@@ -29,6 +30,7 @@ class Music < ActiveRecord::Base
       # pass info to spotify
       if type == "artist"
         data = RSpotify::Artist.find(id)
+        raise
         subarray << self.new(data)
       elsif type == "album"
         data = RSpotify::Album.find(id)
