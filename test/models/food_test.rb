455f53e51fef309310f291287efa47319c63bab7
Leah <leahnpetersen@gmail.com>
2016-05-22 07:21:27 -0700

    created second test for food class

--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -8,7 +8,19 @@ class FoodTest < ActiveSupport::TestCase
 
     results = Food.find(query)
 
-    # a bit pedantic
+    first_result = results[0]
+    key = first_result[0]
+    business = first_result[1]
+
+    assert_equal business.name, 'Fiddlehead Fine Foods & Café'
+  end
+
+  test "food returns expected array in correct format" do
+    query = {
+      '123456789' => { food_id: 'fiddlehead-fine-foods-and-café-seattle' }
+    }
+    results = Food.find(query)
+
     assert_equal results.length, 1
 
     first_result = results[0]
@@ -16,6 +28,5 @@ class FoodTest < ActiveSupport::TestCase
     business = first_result[1]
 
     assert_equal first_result[0], '123456789'
-    assert_equal business.name, 'Fiddlehead Fine Foods & Café'
   end
 end
