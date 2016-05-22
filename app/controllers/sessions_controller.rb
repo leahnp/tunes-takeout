9467cbea2f0b11e807aaab93497c9fe5f8db1089
Leah <leahnpetersen@gmail.com>
2016-05-22 14:51:58 -0700

    cleaned up sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,23 @@
 class SessionsController < ApplicationController
-
-
-
-#   new: shows a view with OAuth sign-in link
+  # new: shows a view with OAuth sign-in link
   def new
   end
-# create: accepts OAuth information from Spotify, finds or creates a User account, and sets user_id in session
-    def create
-    auth_hash = request.env['omniauth.auth']
-    # raise
-    @user = User.find_or_create_from_omniauth(auth_hash)
-  #   if auth_hash["uid"]
-  #     @user = User.find_or_create_from_omniauth(auth_hash)
-      if @user
-        session[:user_id] = @user.id
-        redirect_to root_path
-      else
-        redirect_to root_path, notice: "Failed to save the user"
-      end
-    end
 
-  
+  # create: accepts OAuth information from Spotify, finds or creates a User account, and sets user_id in session
+  def create
+  auth_hash = request.env['omniauth.auth']
+  @user = User.find_or_create_from_omniauth(auth_hash)
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to root_path, notice: "Failed to save the user"
+    end
+  end
+
 # destroy: deletes user_id from session
   def destroy
     session.delete(:user_id)
     redirect_to root_path
   end
-
-
 end
